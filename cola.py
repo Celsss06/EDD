0915f988ec2910e39d40f6ff28b0cc27fc2504f1
Celsss06 <126788535+Celsss06@users.noreply.github.com>
2023-10-16 00:33:07 -0600

    Add files via upload

---
+++ cola.py
@@ -0,0 +1,15 @@
+#se crea la cola y se utiliza una lista
+cola=["1", "2", "3", "4", "5", "6","7", "8", "9", "10"]
+
+#se agregan elementos al final de la cola
+cola.append("11")
+cola.append("12")
+
+print(cola)
+
+#sacar los elementos por donde empieza la cola
+k=cola.pop(0)
+print(f"el primer numero en salir es {k}")
+
+
+
